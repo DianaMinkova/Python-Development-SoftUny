7a4f60353f99ac860942ea79fe18933c704c07b4
Диана Минкова <dianaminkova@yahoo.com>
2022-05-23 13:19:23 +0300

    Update 04. Vacation books list.py

--- Python_Basics/First Steps in Coding - Exercise/04. Vacation books list.py
+++ Python_Basics/First Steps in Coding - Exercise/04. Vacation books list.py
@@ -1,3 +1,4 @@
+# First solution:
 number_of_pages = int(input())
 read_pages_for_one_hour = int(input())
 number_of_days = int(input())
@@ -7,3 +8,17 @@ hour_of_days = total_time_for_read / number_of_days
 
 print(int(hour_of_days))
 
+# Second solution:
+def total_time(pages, hour=1):
+    read = int(pages / hour)
+    return read
+
+
+def days_read(read, day_n):
+    hour_days = int(read / day_n)
+    return hour_days
+
+
+time = total_time(int(input('Input number of pages: ')), int(input('Pages you read in 1 hour: ')))
+days = days_read(time, int(input('The number of days: ')))
+print(f'Hours required: {days}')
